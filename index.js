d63fe7a1118bbcd9b53ee047d7f3e07b918aff60
Martin Schädler <github@alien.de>
2023-05-16 14:49:06 +0300

    headless new

--- index.js
+++ index.js
@@ -99,9 +99,9 @@ function takeScreenshot(argv) {
                 height: argv.height,
             },
             bindAddress: '0.0.0.0',
+            headless: 'new',
             args: [
-                '--no-sandbox',
-                '--headless',
+                '--no-sandbox',                
                 '--disable-gpu',
                 '--disable-dev-shm-usage',
                 '--remote-debugging-port=9222',
