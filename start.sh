c9948e6fb8b9b5d6ed24df62a6f987ede26174b5
Martin Schädler <github@alien.de>
2023-06-14 23:34:46 +0200

    test

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export DBUS_SESSION_BUS_ADDRESS=$(dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address)
+/usr/bin/chromium-browser  --no-sandbox --disable-setuid-sandbox --disable-dev-shm-usage &
 node server.js
